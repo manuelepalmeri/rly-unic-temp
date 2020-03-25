1929ff35b205c9eea57aed9344a56aff7ef1e911
Phaenrir <francesco.festorazzi@studenti.unimi.it>
2020-03-25 16:47:43 +0100

    Remind to send email when a case is closed

--- force-app/main/default/lwc/confirmationModal/confirmationModal.js
+++ force-app/main/default/lwc/confirmationModal/confirmationModal.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
-
-export default class ConfirmationModal extends LightningElement {}
+import { LightningElement,api } from 'lwc';
+export default class ConfirmationModal extends LightningElement {
+    @api title;
+    @api message;
+    @api checkbox;
+    @api confirm;
+    @api cancel;
+    modal;
+    disabledConfirm;
+    @api
+    openModal(){
+        this.modal=true;
+        this.disabledConfirm=true;
+    }
+    handleConfirm(event){  
+        this.disabledConfirm=!(event.target.checked);
+    }
+    @api
+    closeModal(){
+        this.modal=false;
+        if (!this.disabledConfirm) this.dispatchEvent(new CustomEvent('closed'));
+    }
+}
--- force-app/main/default/lwc/testBaseComponent/testBaseComponent.js
+++ force-app/main/default/lwc/testBaseComponent/testBaseComponent.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
-
-export default class TestBaseComponent extends LightningElement {}
+import { LightningElement, api } from 'lwc';
+import closeCase from '@salesforce/apex/caseClosure.closeCase';
+import CCC_CaseRemindTitle from '@salesforce/label/c.CCC_CaseRemindTitle';
+import CCC_CaseRemindMessage from '@salesforce/label/c.CCC_CaseRemindMessage';
+import CCC_CaseRemindCheckbox from '@salesforce/label/c.CCC_CaseRemindCheckbox';
+import CCC_CaseRemindConfirm from '@salesforce/label/c.CCC_CaseRemindConfirm';
+import CCC_CaseRemindCancel from '@salesforce/label/c.CCC_CaseRemindCancel';
+export default class ConfirmationModal extends LightningElement {
+    @api recordId;
+    label={CCC_CaseRemindTitle,CCC_CaseRemindMessage,CCC_CaseRemindCheckbox,CCC_CaseRemindConfirm,CCC_CaseRemindCancel};
+    handleModal(){
+        this.template.querySelector('c-confirmation-modal').openModal();
+    }
+    confirmClosure(){
+        closeCase({caseId:this.recordId})
+        .catch(error => {
+            this.error = error;
+        });
+    }
+}
